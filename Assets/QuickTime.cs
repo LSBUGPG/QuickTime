6e929711244fde83045b0354e44225fad3234599
Paul Sinnett <paul.sinnett@gmail.com>
2021-05-04 16:04:40 +0100

    Initial commit

---
+++ Assets/QuickTime.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickTime : MonoBehaviour
+{
+    public float timeLimit = 1.0f;
+    public float minTime = 2.0f;
+    public float maxTime = 4.0f;
+
+    public GameObject prompt;
+
+    void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText()
+    {
+        yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+
+        prompt.SetActive(true);
+        bool pressed = false;
+
+        while (timeLimit > 0.0f)
+        {
+            timeLimit -= Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                pressed = true;
+                break;
+            }
+            yield return null;
+        }
+
+        prompt.SetActive(false);
+
+        if (pressed)
+        {
+            Debug.Log("Well done!");
+        }
+        else
+        {
+            Debug.Log("oops");
+        }
+    }
+}
